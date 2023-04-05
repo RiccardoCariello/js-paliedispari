d4f9c3f230721364b43196431a23551659211a65
MillionDollarBob <riccardo.c1995@outlook.com>
2023-04-05 15:21:46 +0200

    aggiunti commenti

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -6,10 +6,12 @@ Creare una funzione per capire se la parola inserita è palindroma.
 
 //--------------------------- Main --------------------------------
 let parolaTest;
+
+//faccio mettere l'input finchè non è valido
 do{
     parolaTest = prompt("Inserisci la parola da testare! un numero non è idoneo.");
 }while(!isNaN(parolaTest));
-
+//invoco la funzione e stampo il risultato sia a console che nell'alert
 console.log(palindromoCheck(parolaTest));
 alert(palindromoCheck(parolaTest));
 
@@ -27,12 +29,15 @@ alert(palindromoCheck(parolaTest));
 
 
 function palindromoCheck(parola){
-
+    //istanzio una variabile momentanea che mi serve solo per il test
     let reversedParola = "";
+    //normalizzo la stringa da testare
     parola = parola.toLowerCase();
+    //partendo dalla fine immagazzino i caratteri uno per uno partendo dalla fine nella mia variabile d'appoggio
     for(i=parola.length-1 ; i >= 0 ; i--){
         reversedParola += parola.charAt(i);
     }
+    //se le due parole sono uguali allora la stringa è palindroma
     if(parola == reversedParola){return "La parola data è palindroma";}
     else{return "La parola data non è palindroma";}
 
